74fdaee841c480f37fc84c7211b6554dfafffc9e
uditshaw <uditshaw17@gmail.com>
2023-07-06 22:06:45 +0530

    Added Network repo

---
+++ app/src/main/java/com/example/marvelcharactersapp/model/api/MarvelApiRepo.kt
@@ -0,0 +1,37 @@
+package com.example.marvelcharactersapp.model.api
+
+import com.example.marvelcharactersapp.model.CharactersApiResponse
+import kotlinx.coroutines.flow.MutableStateFlow
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+// this class will contain a flow. characters variable is a flow. NetworkResult.Initial() is used for initializing the mutableStateFlow
+class MarvelApiRepo(private val api: MarvelApi) {
+    val characters = MutableStateFlow<NetworkResult<CharactersApiResponse>>(NetworkResult.Initial())
+
+    fun query(query: String){
+        characters.value = NetworkResult.Loading()
+        api.getCharacters(query).enqueue(object : Callback<CharactersApiResponse>{
+            override fun onResponse(
+                call: Call<CharactersApiResponse>,
+                response: Response<CharactersApiResponse>
+            ) {
+                if (response.isSuccessful)
+                    response.body()?.let {
+                        characters.value = NetworkResult.Success(it)
+                    }
+                else
+                    characters.value = NetworkResult.Error(response.message())
+            }
+
+            override fun onFailure(call: Call<CharactersApiResponse>, t: Throwable) {
+                t.localizedMessage?.let {
+                    characters.value = NetworkResult.Error(it)
+                }
+                t.printStackTrace()
+            }
+
+        })
+    }
+}
